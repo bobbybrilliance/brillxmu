f50dd7ead68ba994738cc46814a481c6268787ff
bobbybrilliance <bobbybrilliance@gmail.com>
2024-03-17 17:09:32 +0000

    Initial Commit

---
+++ brillxmu/brillxmuApp.swift
@@ -0,0 +1,17 @@
+//
+//  brillxmuApp.swift
+//  brillxmu
+//
+//  Created by Limahl Mills on 17/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct brillxmuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
